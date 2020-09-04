07daf62d0b6a906127875da370438a7ea56b653e
rkarri <ravi-karri@hotmail.com>
2020-09-04 12:19:45 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HttpClientModule} from '@angular/common/http';
+import {StocksService} from './services/stocks.service.ts';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [StocksService]
 })
 export class AppModule { }
