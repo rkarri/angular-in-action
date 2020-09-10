399bf13393b997cfe9bba01be327e8b9833a4ad0
rkarri <ravi-karri@hotmail.com>
2020-09-10 13:45:21 -0500

    2 files modified, 1 added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StocksService, StockInterface } from '../services/stocks.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  stocks: StockInterface[];
+  symbols: string[];
 
-  constructor() { }
+  constructor(private stockSvc: StocksService) {
+    this.symbols = this.stockSvc.get();
+   }
 
   ngOnInit() {
+    this.stockSvc.load(this.symbols).subscribe(stocks => this.stocks = stocks);
   }
 
 }
