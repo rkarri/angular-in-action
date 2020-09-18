8c080a97421fd759f793b8e9205411916ca4d048
rkarri <ravi-karri@hotmail.com>
2020-09-18 12:11:44 -0500

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
@@ -11,9 +11,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ManageComponent } from './manage/manage.component';
 import { AppRoutes } from './app.routes';
 import { MetricComponent } from './metric/metric.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule, AppRoutes ],
+  imports:      [ BrowserModule, CommonModule, FormsModule, HttpClientModule, AppRoutes, NgbModule.forRoot() ],
   declarations: [ AppComponent, HelloComponent, SummaryComponent, DashboardComponent, ManageComponent, MetricComponent ],
   bootstrap:    [ AppComponent ],
   providers: [StocksService]
--- src/app/metric/metric.component.ts
+++ src/app/metric/metric.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-metric',
