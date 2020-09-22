00fee53eff99f66e2891a1d428c7337fd69dbdb6
rkarri <ravi-karri@hotmail.com>
2020-09-22 12:47:01 -0500

    1 file modified

--- src/app/metric/metric.component.ts
+++ src/app/metric/metric.component.ts
@@ -9,13 +9,35 @@ import {FormsModule} from '@angular/forms';
 export class MetricComponent {
   @Input() title: string = '';
   @Input() description: string = '';
-  @Input('used') value: number = 0;
-  @Input('available') max: number = 100;
+  private _value: number = 0;
+  private _max: number = 100;
+
+  @Input('used')
+  set value(value: number) {
+     if(isNaN(value)){
+       value = 0;
+     }
+     this._value = value;
+  }
+
+  get max() : number {
+    return this._max;
+  }
+
+  @Input('available')
+  set max(max: number) {
+     if(isNaN(max)){
+       max = 100;
+     }
+     this._max = max;
+  }
+
+  get value() : number {
+    return this._value;
+  }
 
   isDanger() {
     return this.value / this.max > 0.7;
   }
-
-  constructor() { }
 
 }
